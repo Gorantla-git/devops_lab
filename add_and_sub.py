f126b72be9d087cce13c59cb8fdddfafa0b5363c
Gorantla-git <iesc.chandu@gmail.com>
2024-12-18 11:20:46 -0800

    update code

--- add_and_sub.py
+++ add_and_sub.py
@@ -1,12 +1,12 @@
 a = int (input ("Please enter A number ="))
 b = int (input ("PLease enter B number ="))
 
-print ("A vaule" (a))
-print ("B vaule" (b))
+print (str("A vaule") (a))
+print (str("B vaule") (b))
 
 c = a + b
 
 d = a - b
 
-print ("add output" (c))
-print ("add output" (d))
+print (str("add output") (c))
+print (str("add output") (d))
